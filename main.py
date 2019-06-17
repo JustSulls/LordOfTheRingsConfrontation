9108546578fab8b8f617c19aa979dc4323fbc064
Matt <matthew.geary313@gmail.com>
2019-06-17 18:20:54 -0400

    commit

--- main.py
+++ main.py
@@ -4,10 +4,13 @@ import PySide2.QtCore
 import sys
 
 
+
+
 if __name__ == "__main__":
     app = PySide2.QtWidgets.QApplication([])
     # window = MainWindow()
     # window.show()
     test = FormLOTRConfrontation.FormLOTRConfrontation()
     # test.show()
+    print ("matt")
     sys.exit(app.exec_())
